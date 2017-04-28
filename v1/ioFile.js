f5947b8f4f00220b8835ae7eefe1f35bfafb4ebe
Remaii <remy.thidet@hotmail.fr>
2017-04-28 23:21:26 +0200

    rm ioFile

--- v1/ioFile.js
+++
@@ -1,9 +0,0 @@
-var io = require('socket.io');
-
-var new_membre = function(pseudo) {
-	socket.user_name = pseudo;
-	socket.emit('logged_on', pseudo + ' viens de se connecter');
-};
-
-
-exports.new_membre = new_membre;
